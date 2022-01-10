7d13effb14d5a13ee9bc6cb36a1ffa6a2383d3bf
leoxu0257 <enmacorradi@163.com>
2022-01-11 12:11:14 +1300

    add link to wallet button

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App.jsx";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+// Import ThirdWeb
+import { ThirdwebWeb3Provider } from '@3rdweb/hooks';
+
+// Include what chains you wanna support.
+// 4 = Rinkeby.
+const supportedChainIds = [4];
+
+// Include what type of wallet you want to support.
+// In this case, we support Metamask which is an "injected wallet".
+const connectors = {
+  injected: {},
+};
 
 // Render the App component to the DOM
 ReactDOM.render(
   <React.StrictMode>
+    <ThirdwebWeb3Provider
+      supportedChainIds={supportedChainIds}
+      connectors={connectors}
+    >
     <App />
+    </ThirdwebWeb3Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
