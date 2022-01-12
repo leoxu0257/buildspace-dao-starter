521ba0f7e0d3c6338f41ca1f357315008ce0f24d
leoxu0257 <enmacorradi@163.com>
2022-01-12 14:35:15 +1300

    update config nft

--- scripts/3-config-nft.js
+++ scripts/3-config-nft.js
@@ -2,16 +2,16 @@ import sdk from "./1-initialize-sdk.js";
 import { readFileSync } from "fs";
 
 const bundleDrop = sdk.getBundleDropModule(
-  "INSERT_DROP_MODULE_ADDRESS",
+  "0xf28E408DaF77181fa5CE21bf5d12Ba1D1fE03B4f",
 );
 
 (async () => {
   try {
     await bundleDrop.createBatch([
       {
-        name: "Leaf Village Headband",
-        description: "This NFT will give you access to NarutoDAO!",
-        image: readFileSync("scripts/assets/headband.png"),
+        name: "AllenBot",
+        description: "This NFT will give you access to AllenDAO!",
+        image: readFileSync("scripts/assets/Allen.png"),
       },
     ]);
     console.log("✅ Successfully created a new NFT in the drop!");
